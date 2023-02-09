44ec8420a63673ca563d927c0f1ab2877d3c1fe8
xsrole <xsrole@outlook.com>
2023-02-09 09:58:38 +0800

    --up 23.2.9

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-import { createServer } from 'node:http';
-import { createApp, eventHandler, toNodeListener, createRouter } from 'h3';
-const app = createApp();
-const port = process.env.PORT || 3000;
-const router = createRouter();
+import { createServer } from 'node:http'
+import { createApp, createRouter, eventHandler, toNodeListener } from 'h3'
+const app = createApp()
+const port = process.env.PORT || 3000
+const router = createRouter()
 router.get(
   '/',
   eventHandler(() => {
-    return 'hello world';
+    return 'hello world'
   }),
-);
-app.use(router);
+)
+app.use(router)
 createServer(toNodeListener(app)).listen(port, undefined, () => {
-  console.log(`server is runing at http://127.0.0.1:${port}/`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`server is runing at http://127.0.0.1:${port}/`)
+})
